c559f1e5b9e5c0627657f8b09e65446b6fcb74ee
Zulfannyzahra <aya554933@gmail.com>
2025-03-14 09:54:46 +0700

    petantang petenteng

---
+++ src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val friends = mutableListOf("Khansa", "Izuka", "Rizqia")
+    friends.remove("Izuka")
+//    ini buat ngehapus index 1
+    println(friends)
+}
---
+++ src/wowowo.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val katabangrehanterserah = mapOf(
+        "mw" to "ak jg mw"
+        "ap" to "apa coba"
+    )
+    println(katabangrehanterserah)
+}
